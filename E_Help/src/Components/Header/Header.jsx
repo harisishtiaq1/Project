3abe5719ed78fd31436154b41dae8b9a65f4171d
harisishtiaq1 <harisharry232@gmail.com>
2023-01-17 13:43:12 +0500

    Changes

--- E_Help/src/Components/Header/Header.jsx
+++ E_Help/src/Components/Header/Header.jsx
@@ -27,9 +27,6 @@ function Header() {
                 <Link to = '/'>  
                 <img src = {logo} alt = 'logo' />
                </Link>
-               <div className="text-center left">
-                            <span><a onClick={()=>signup()} href="">About Us</a></span>
-                        </div>
             </div>
             
             <div className="header-middle">
@@ -48,25 +45,13 @@ function Header() {
                             <span><a onClick={()=>contact()} href="">Contact Us</a></span>
                         </div>
                         
-            <div className="header-right">
-            <div className="header-right-container">
-              < div className="icon" /> 
-              <AiOutlineInbox size={25} className="icon"/>
-              <svg
-              aria-hidden="true"
-              className="svg-icon iconStackExchange"
-              width="24"
-              height="24"
-              viewBox="0 0 18 18"
-              fill="rgba(0,0,0,0.5)"
-              style={{
-                cursor: "pointer",
-              }}
-            >
-              <path d="M15 1H3a2 2 0 00-2 2v2h16V3a2 2 0 00-2-2ZM1 13c0 1.1.9 2 2 2h8v3l3-3h1a2 2 0 002-2v-2H1v2Zm16-7H1v4h16V6Z"></path>
-            </svg>
-            </div>
-            </div>
+                        <div class="dropdown">
+                          <span><i class="fa-solid fa-user icon"></i></span>
+                        <div class="dropdown-content">
+                          <p>Sign out</p>
+                          <p>Profile</p>
+                            </div>
+                            </div>
           </div>
         </header>
     )
