1d7f90f7378ec7257ebc4a11ea8724765bd7a19b
harisishtiaq1 <harisharry232@gmail.com>
2023-01-17 12:16:58 +0500

    Changes

--- src/api/models/Answer.model.js
+++ src/api/models/Answer.model.js
@@ -3,6 +3,6 @@ const answerSchema = new mongoose.Schema({
   createdBy: { type: mongoose.Schema.Types.ObjectId,required: true, ref: 'User' },
   questionId: {type: mongoose.Schema.Types.ObjectId,required:true,ref: 'Question' },
   comments: {type:String,require:true},
-});
+},{ timestamps: true });
 
 module.exports = mongoose.model("Answer", answerSchema);
